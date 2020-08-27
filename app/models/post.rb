0a224856381b6c71a8eef3a398c84796f4d3d3e9
AmSmo <adam.smolenski@gmail.com>
2020-08-27 13:45:52 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+    CATS= ["Fiction", "Non-Fiction"]
+    validates :title, presence: true
+    validates :content, length: { minimum: 100 }
+    validates :category, inclusion: CATS
 end
